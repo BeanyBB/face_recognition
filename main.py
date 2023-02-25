9deb5ee004f80bb932584823e42cd188c5210874
Bean <ben.blankenbiller@gmail.com>
2023-02-25 16:38:36 -0600

    now is able to detect webcam and display it

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import cv2
+import numpy as np
 
-def main():
-    pass
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows
+
